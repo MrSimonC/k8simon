fd486921f30ebac837fa89b33cc2b8f4795cbab4
Simon C <iamontheinternet@yahoo.com>
2022-04-13 09:28:49 +0100

    move menu to layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -643,64 +643,6 @@ export default {
         }; // end ssAlertBoxes
 
 
-    /* Mobile Menu
-        * ---------------------------------------------------- */ 
-        const ssMobileMenu = function() {
-
-            const $navWrap = $('.s-header__nav-wrap');
-            const $closeNavWrap = $navWrap.find('.s-header__overlay-close');
-            const $menuToggle = $('.s-header__toggle-menu');
-            const $siteBody = $('body');
-            const $simonCloseOnAnyMenuItem = $navWrap.find('.s-header__nav');
-            
-            $menuToggle.on('click', function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-
-                $siteBody.addClass('nav-wrap-is-visible');
-            });
-
-            $closeNavWrap.on('click', function(e) {
-                e.preventDefault();
-                e.stopPropagation();
-            
-                if($siteBody.hasClass('nav-wrap-is-visible')) {
-                    $siteBody.removeClass('nav-wrap-is-visible');
-                }
-            });
-
-            // open (or close) submenu items in mobile view menu. 
-            // close all the other open submenu items.
-            $('.s-header__nav .has-children').children('a').on('click', function (e) {
-                e.preventDefault();
-
-                if ($(".close-mobile-menu").is(":visible") == true) {
-
-                    $(this).toggleClass('sub-menu-is-open')
-                        .next('ul')
-                        .slideToggle(200)
-                        .end()
-                        .parent('.has-children')
-                        .siblings('.has-children')
-                        .children('a')
-                        .removeClass('sub-menu-is-open')
-                        .next('ul')
-                        .slideUp(200);
-                }
-            });
-
-            // gridsome doesn't navigate with a page refresh, so we need to manually remove the menu overlay
-            $simonCloseOnAnyMenuItem.on('click', function(e) {
-                e.stopPropagation();
-            
-                if($siteBody.hasClass('nav-wrap-is-visible')) {
-                    $siteBody.removeClass('nav-wrap-is-visible');
-                }
-            });
-
-        }; // end ssMobileMenu
-
-
     /* search
         * ------------------------------------------------------ */
         const ssSearch = function() {
@@ -905,7 +847,7 @@ export default {
 
             ssAlertBoxes();
             ssSearch();
-            ssMobileMenu();
+            
             ssMasonry();
             //ssBricksAnimate();
             ssSlickSlider();
